286edf6a265cae91f739efd677ffa45988222ff3
tyler <tarthurf@yahoo.com>
2021-09-28 13:46:44 -0700

    general layout made, modal created, important links added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 console.log("hello world")
+const hamburgerBtn = $("#hamburger")
+const hamburgerCheckEl = $("#hamburger-check")
+const modalEl = $("#modal")
+modalEl.hide()
+
+hamburgerBtn.click(() => {
+  modalEl.is(":hidden") ? modalEl.show() : modalEl.hide()
+})
